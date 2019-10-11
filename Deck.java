5080eb25a4b80b5f7974ec2c2963f8be9c84b3fc
ramalaitu10 <55300191+ramalaitu10@users.noreply.github.com>
2019-10-11 17:46:55 +0200

    gameplay and deck

--- Deck.java
+++ Deck.java
@@ -17,14 +17,33 @@ public class Deck{
       //accept cards
 	  
 	  Suite[] v = Suite.values(); //this gives an array of suite values
-	  System.out.println(Arrays.toString(Suite.values()));
+	  //System.out.println(Arrays.toString(Suite.values()));
 	  for (int j = 0; j < v.length; j++){
 		  //System.out.println(suite);
-		  for (int i = 0; i < 13; i++){
-			  deck.add(new Card(Integer.toString(i+1), v[j]));
+		  for (int i = 1; i < 14; i++){
+			  if(i>=2 && i<=10){
+				deck.add(new Card(Integer.toString(i), v[j]));
+			  }
+			  else{
+				  String a = "";
+				  if(i==1){
+					   a = "A";
+				  }
+				  if(i==11){
+					  a = "J";
+				  }
+				  if(i==12){
+					  a = "Q";
+				  }
+				  if(i==13){
+					  a = "K";
+				  }
+				  deck.add(new Card(a, v[j]));
+			  }
 			  //this.deck.get(i).setSuite(Suite.values()[j]);
 			  //System.out.println(deck.get(i));
 		  }
+		 
 		  
 	  }
       //create deck using arrayList
@@ -37,40 +56,41 @@ public class Deck{
        
 		Random random = new Random();
 		int x = 20 + random.nextInt(10);
-		int y = 52 - x;
+		int y = this.deck.size() - x;
 		Card[] first = new Card[x];
 		Card[] second = new Card[y];
 		
 		//making the two arrays for shuffling
 		for(int j = 0;j < x;j++){
-			first[j] = this.deck[j];
+			first[j] = this.deck.get(j);
 		}
 		
 		for(int k = 0;k < y;k++){
-			second[k] = this.deck[k+x];
+			second[k] = this.deck.get(k+x);
 		}
 		
 		int i = 0,m = 0,n = 0;
-		
+		//System.out.println("M here");
 		//shuffling starts here
-		while(i <52){
-			
+		while(i < 52){
+			//System.out.println("M here");
 			int p = random.nextInt(2)+1;
 			
 			if(p == 1 && m<first.length){
-				this.deck[i] = first[m];
+				this.deck.add(i,first[m]) ;
 				//System.out.println(this.deck[i].toString());
 				i++;
 				m++;
 			}
 			
 			else if(p==2 && n< second.length){
-				this.deck[i] = second[n];
+				this.deck.add(i,second[n]);
 				//System.out.println(this.deck[i].toString());
 				i++;
 				n++;
 			}
 		}
+		//System.out.println("M here");
 
 	}
    
@@ -105,15 +125,15 @@ public class Deck{
     
    }
    
-  /*  public ArrayList<Card> serve(int n){//n depends on the function of the serve
+   public ArrayList<Card> serve(int n){//n depends on the function of the serve
 	   //serve stipulated number of cards to the calling object
-	   ArrayList<Card> s;
+	   ArrayList<Card> s = new ArrayList<Card>(n);
 	   for(int i = 0; i < n; i++){
 		   s.add(this.deck.get(i));
 		   this.deck.remove(i);
 	   }
 	   return s;
-   } */
+   }
    
    
    
--- game.java
+++ game.java
@@ -15,8 +15,9 @@ public class game{
 		D = new Deck();
 		D.Shuffle();
 		//D.getCard(1);
+		drawPile = new ArrayList<Card>(12);
 		Discard = new ArrayList<Card>(12);
-		//this.drawPile.addAll(D);
+		this.drawPile.addAll(D.serve(51));
 		in = new Scanner(System.in);
 		playe();
 		
@@ -37,12 +38,13 @@ public class game{
 		}
 		
 		player = getPlayer(0);
-		Discard.add(this.drawPile.get(0));
+		Discard.add(D.serve(1).get(0));
 		while (!isDone()) {
 			//System.out.println("It works!!!");
 			Playgame(player);
 			player = nextPlayer(player);
 		}
+		System.out.println(player +" Won!!!");
 	
 	}
 	//checks if the game is done
@@ -82,18 +84,85 @@ public class game{
 	}
 	//player plays
 	public void Playgame(Player player){
-		System.out.println("Its " + player.getName()+"'s turn");
-		System.out.println("Select Card!");
-		int x = in.nextInt();
-		for(int i = 0;i< player.getHand().size();i++){
-			System.out.println("M in!!! " + Discard.get(0));
+		
+		System.out.println("\nIts " + player.getName()+"'s turn\n");
+		System.out.println("Discard card is: "+this.Discard.get(this.Discard.size()-1));
+		
+		System.out.println("\nThis is your hand!!");
+		
+		for(int i =0;i<player.getHand().size();i++){
+			System.out.println(i + " = " + player.getHand().get(i));
 		}
+		System.out.println("Select Card! or 20 if you don't have it");
+		
+		int x = in.nextInt();
+		if(drawPile.size()==0){
+				reshuffle();
+				
+			}
+		while(x >= player.getHand().size()){
+			
+			player.getHand().add(drawPile.get(0));
+			this.drawPile.remove(drawPile.get(0));
+			
+			Playgame(nextPlayer(player));
+		}
+		
+		if(cardMatches(this.Discard.get(this.Discard.size()-1),player.getHand().get(x))){
+			
+			System.out.println(this.Discard.get(this.Discard.size()-1)+ " M in!!! "+ player.getHand().get(x));
+			this.Discard.add(player.getHand().get(x));
+			Card d = player.getHand().get(x);
+			player.getHand().remove(player.getHand().get(x));
+			//Suppose to skip a player
+			
+			if(d.getRank().equals("7") && !isDone()){
+				this.player = nextPlayer(player);
+			}
+			//suppose to choose a suit
+			else if(d.getRank().equals("8") && !isDone()){
+				System.out.println("Choose suit: ");
+			
+			}
+			//supposed to reverse
+			else if(d.getRank().equals("J") && !isDone()){
+				int y = playerList.indexOf(player);
+				if(y >= 1){
+					Playgame(playerList.get(y-1));
+				}
+				else{
+					Playgame(playerList.get(playerList.size()-1));
+				}
+			
+			}
+		}
+	
+	}
+	public boolean cardMatches(Card discard,Card that) 
+	{
+		if (discard.getSuite().equals(that.getSuite())) 
+		{
+			return true;
+		}
+		if (discard.getRank().equals(that.getRank()))
+		{
+			return true;
+		}
+		if (that.getRank().equals("8")) 
+		{
+			return true;
+		}
+		return false;
 	}
 	
 	//get the next playing player
 	public Player nextPlayer(Player currentP){
-		if(playerList.indexOf(currentP)+1< playerList.size()){
+		if(playerList.indexOf(currentP)+1< playerList.size()&& (!isDone())){
 			return playerList.get(playerList.indexOf(currentP)+1);
+		}
+		if(isDone()){
+			return currentP;
+			
 		}
 		return playerList.get(0);
 	}
